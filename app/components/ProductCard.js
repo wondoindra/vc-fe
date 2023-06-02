075a67f9f268f8850ad4fc2b08ffcdc390030fd3
Wondo Indra <wondoindra28@gmail.com>
2023-06-02 15:34:20 +0700

    addd product card

---
+++ app/components/ProductCard.js
@@ -0,0 +1,33 @@
+import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from "@mui/material/Typography"
+
+const ProductCard = ({ product, key }) => {
+  return (
+    <Grid item key={key} xs={12} sm={6} md={3}>
+      <Card
+        sx={{ height: '100%', display: 'flex', flexDirection: 'column', cursor: 'pointer' }}
+      >
+        <CardMedia
+          component="div"
+          sx={{ pt: '80.25%' }}
+          image={product.source}
+        />
+        <CardContent sx={{ flexGrow: 1 }}>
+          <Typography gutterBottom variant="subtitle1" sx={{ lineHeight: 'normal' }}>
+            {product.name}
+          </Typography>
+          <Typography variant="subtitle1" sx={{ fontWeight: 'bold', lineHeight: 'normal' }}>
+            {product.price}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  )
+}
+
+export default ProductCard
