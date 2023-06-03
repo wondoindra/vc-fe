468d355df86f30ccb3dde9d722fdd834709598c0
Wondo Indra <wondoindra28@gmail.com>
2023-06-04 00:42:13 +0700

    fix api param

--- app/api/admin.js
+++ app/api/admin.js
@@ -2,7 +2,7 @@ import api from './api'
 
 const login = data => api.post('/admin/login', data)
 const getUsers = () => api.get('/admin/users')
-const verifyUser = id => api.post('/admin/verify/id')
+const verifyUser = id => api.post(`/admin/verify/${id}`)
 
 const adminApis = {
   login,
