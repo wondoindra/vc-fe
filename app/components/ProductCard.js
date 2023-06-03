a9a22f641d063b5c7216672b2a8b208394e33721
Wondo Indra <wondoindra28@gmail.com>
2023-06-04 00:42:20 +0700

    remove unused

--- app/components/ProductCard.js
+++ app/components/ProductCard.js
@@ -1,7 +1,5 @@
-import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
-import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from "@mui/material/Typography"
